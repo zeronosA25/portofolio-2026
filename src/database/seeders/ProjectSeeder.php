bfcb79bd0e9b44329deb330ebc16eb8161e601e0
root <root@ADITYA.localdomain>
2026-05-25 14:52:39 +0700

    Tambahkan Laporan

--- src/database/seeders/ProjectSeeder.php
+++ src/database/seeders/ProjectSeeder.php
@@ -16,8 +16,8 @@ class ProjectSeeder extends Seeder
                 'description' => 'Website ini merupakan website portofolio personal yang dibuat untuk menampilkan profil, kemampuan, dan project yang sedang dikembangkan oleh penulis. Website ini menyediakan beberapa halaman utama seperti Home/About, Showcase/Project, dan Contact. Pada halaman Showcase/Project, pengguna dapat melihat informasi mengenai laporan awal project akhir yang berisi judul project, deskripsi singkat, analisis masalah, kebutuhan sistem, teknologi yang digunakan, serta rancangan diagram sistem.
                 Website ini dibuat secara dinamis sehingga data project, skill, dan pesan kontak dapat dikelola melalui halaman admin. Dengan adanya website ini, informasi mengenai profil dan perkembangan project dapat ditampilkan secara lebih terstruktur, profesional, dan mudah diakses.',
                 'image' => 'images',
-                'erd_image' => 'images/ERD.png',
-                'flowchart_image' => 'images/FlowChart.png',
+                'erd_image' => 'images',
+                'flowchart_image' => 'images',
                 'demo_url' => null,
                 'github_url' => 'https://github.com/zeronosA25',
                 'technologies' => ['Laravel', 'Filament v3', 'Livewire', 'Blade', 'MariaDB'],
